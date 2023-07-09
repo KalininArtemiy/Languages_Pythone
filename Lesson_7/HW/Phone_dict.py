13528554fb4553146923ca2457a1b2a5ccdb51b2
Kalinin <9776020@mail.ru>
2023-07-09 12:57:32 +0300

    Phone directory reading

--- Lesson_7/HW/Phone_dict.py
+++ Lesson_7/HW/Phone_dict.py
@@ -1,14 +1,29 @@
-""" def read_file():
-    pass
+def read_file():
+  action = int(input(f'''Enter 1 to show all telephone directory, enter 2 to find Surname, Name, patronymic or number: '''))
+  if action == 1:
+     with open('my_file.txt', 'r',  encoding='utf-8') as f:
+        contacts = f.read()
+        print(*(contacts.split(';')))
+  elif action == 2: 
+     find_contact = str(input("Enter surname: ")).upper()
+     with open('my_file.txt', 'r',  encoding='utf-8') as f:
+        contacts = f.readlines()
+        for contact in contacts:
+           if find_contact in contact.upper().split(';'):
+              print(*contact.split(';'), end = "")
+  else: print('Unknown command')
+  return()
+           
+     
 
 
 def write_file():
-    pass
+   pass
 
 
 def menu():
-    comman_menu = int(input("Enter 1 for reading phone number, 2 for writing new number, 3 for changing ")) """
+    command_menu = int(input("Enter 1 for reading phone number, 2 for writing new number, 3 for changing "))
+    if command_menu == 1:
+        read_file()
 
-with open('my_file.txt', 'r') as f:
-  st = f.read()
-print(st)
+menu()
