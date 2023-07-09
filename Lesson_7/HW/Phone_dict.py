5fd61daeb73e16c07ae909dd9f5fa60fca73be95
Kalinin <9776020@mail.ru>
2023-07-09 11:18:44 +0300

    HW

---
+++ Lesson_7/HW/Phone_dict.py
@@ -0,0 +1,14 @@
+""" def read_file():
+    pass
+
+
+def write_file():
+    pass
+
+
+def menu():
+    comman_menu = int(input("Enter 1 for reading phone number, 2 for writing new number, 3 for changing ")) """
+
+with open('my_file.txt', 'r') as f:
+  st = f.read()
+print(st)
